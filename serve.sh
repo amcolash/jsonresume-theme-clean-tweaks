398c37d9f31eeb39f4a595e4c725867deab04828
Andrew McOlash <amcolash@gmail.com>
2018-03-25 15:57:31 -0500

    check for nodemon with serve

--- serve.sh
+++ serve.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if ! [ -x "$(command -v nodemon)" ]; then
+  echo 'Error: nodemon is not installed, please install via "npm install -g nodemon"' >&2
+  exit 1
+fi
+
 nodemon -e json,hbs,css,js index.js
